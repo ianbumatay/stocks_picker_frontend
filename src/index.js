f4f6a13ba72dad51194db7a247220d88fcf600ac
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-06-21 14:15:25 -0400

    import all dependencies in index.js file and create store and pass it to Provider component as a prop

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
 
-ReactDOM.render(
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'thunk'
+import {Provider} from 'react-redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import {BrowserRouter as Router} from 'react-router-dom'
 
-    <App />,
+
+const store = createStore(reducer, composeWithDevtools(applyMiddleware(thunk)))
+
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>,
  
   document.getElementById('root')
 );
