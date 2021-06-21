8ffddaefe2a794411b4664e86cc7efd9d83226e3
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-06-21 13:32:06 -0400

    build Container StockPage and render to App container

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 
 import './App.css';
+import StockPage from './Containers/StockPage'
 import {Component} from 'react';
 
-class App extends Component {
+class App extends Component {  
+
+
+
+  // componentDidMount(){
+  //   fetch('http://localhost:3000/api/v1/stocks')
+  //   .then(resp => resp.json())
+  //   .then( data => console.log(data))
+  // }
 
   render(){
     return(
       <div className="App">
-          App Component
+          <StockPage/>
       </div>
     )
   }
---
+++ src/Containers/StockPage.js
@@ -0,0 +1,14 @@
+import {Component} from 'react' 
+
+class StockPage extends Component {
+
+    render(){
+        return(
+            <div>
+                Stocks Page
+            </div>
+        )
+    }
+} 
+
+export default StockPage;
