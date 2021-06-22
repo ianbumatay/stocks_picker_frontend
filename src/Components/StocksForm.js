1ca7131620e991e08263dbb3f3bcaf98a631b9e7
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-06-22 09:29:26 -0400

    sets up initial state and controlled component and created handleChange function.

--- src/Components/StocksForm.js
+++ src/Components/StocksForm.js
@@ -2,11 +2,23 @@ import React, {Component} from 'react'
 
 class StocksForm extends Component {
 
+    state = { name: '', symbol: '', price: '' } 
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+
+
     render(){
         return(
             <div>
                 <form>
-                    <input/>
+                    <input type="text" placeholder="Symbol" name="symbol" value={this.state.symbol} onChange={this.handleChange}/>
+                    <input type="text" placeholder="Name" name="name" value={this.state.name} onChange={this.handleChange}/> 
+                    <input type="text" placeholder="price" name="price" value={this.state.price} onChange={this.handleChange}/>
                 </form>
             </div>
         )
