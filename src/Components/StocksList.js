b0259ac5b19421dfe2ee8965cc9e4bd7ce56763a
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-06-23 08:49:02 -0400

    created search component and render it to StocksList component

--- src/Components/StocksList.js
+++ src/Components/StocksList.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import Search from './Search'
 
 const StocksList = (props) => {
     console.log(props)
 
     return(
        <div>
+           <Search/>
            {props.stocks.map( (stock) => 
               <div key={stock.id}> 
                  <Link to={`/stocks/${stock.id} `} > {stock.symbol} - {stock.name} - {stock.price} </Link>
