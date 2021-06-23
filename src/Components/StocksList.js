849ef006e8a31f18dae516458b6eaac3e2d2d3fb
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-06-22 22:33:17 -0400

    created Linkt to stock component

--- src/Components/StocksList.js
+++ src/Components/StocksList.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import {Route, Link} from 'react-router-dom'
 
 const StocksList = (props) => {
     console.log(props)
 
     return(
-       <div>{props.stocks.map( (stock) => <ol> {stock.symbol} - {stock.name} - {stock.price}</ol>)}</div>
+       <div>
+           {props.stocks.map( (stock) => 
+              <div> 
+                 <Link to="`/stocks/${stock.id}`"> {stock.symbol} - {stock.name} - {stock.price} </Link>
+              </div> 
+            )} 
+       </div>
     )
 } 
 
